4ad10b502b53a71d633d2b82a1c432c77e3f2f63
Lester <lhernandez@hmd.consulting>
2020-08-01 14:51:01 -0600

    Primer Commit

---
+++ src/main/java/com/udemy/app/SpringMvcUdemyPruebaConceptoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcUdemyPruebaConceptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcUdemyPruebaConceptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/app/SpringMvcUdemyPruebaConceptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcUdemyPruebaConceptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
