cfb9db9ed7f32c072867eb9bda4171e93fdaf4a5
Lester <lhernandez@hmd.consulting>
2020-09-10 11:00:07 -0600

    Valores Necesarios Para la Pauta

--- src/main/java/com/udemy/controller/rest/ModeloRestController.java
+++ src/main/java/com/udemy/controller/rest/ModeloRestController.java
@@ -1,5 +1,9 @@
 package com.udemy.controller.rest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -7,11 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.controller.response.ErrorRestResponse;
+import com.udemy.model.Modelo;
+import com.udemy.service.IMarcaService;
 import com.udemy.service.IModeloService;
 
 @RestController
@@ -20,6 +26,9 @@ public class ModeloRestController {
 
 	@Autowired
 	IModeloService service;
+
+	@Autowired
+	IMarcaService marcaService;
 
 	@GetMapping(value = "/getCurrentId", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> getCurrentId() throws Exception {
@@ -37,6 +46,29 @@ public class ModeloRestController {
 	public ResponseEntity<?> isCodeUsed(@PathVariable("code") String code) throws Exception {
 		try {
 			return new ResponseEntity<Boolean>(service.getByCodigo(code) == null ? false : true, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<ErrorRestResponse>(
+					new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+//	@GetMapping("/getModelosByMarca")
+//	public  Map<String, String> getStateCityValues(@RequestParam("modelo.marca") Integer idMarca) {
+//		Map<String, String> modelosValues = new HashMap<>();
+//		List<Modelo> modelos = marcaService.listarPorId(idMarca).getModelos();
+//
+//		modelos.forEach(modelo -> modelosValues.put(String.valueOf(modelo.getId()), modelo.getModelo()));
+//
+//		return modelosValues;
+//	}
+
+	@GetMapping("/getModelosByMarca")
+	public ResponseEntity<?> getModelosByMarca(@RequestParam("modelo.marca") Integer idMarca) throws Exception {
+		try {
+			return new ResponseEntity<Map<String, String>>(ToHashMap(marcaService.listarPorId(idMarca).getModelos()),
+					HttpStatus.OK);
+
 		} catch (Exception e) {
 			return new ResponseEntity<ErrorRestResponse>(
 					new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
@@ -64,5 +96,16 @@ public class ModeloRestController {
 					new ErrorRestResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+
+	private Map<String, String> ToHashMap(List<Modelo> modelos) {
+		Map<String, String> modelosValues = new HashMap<>();
+		modelosValues.put("0", "[Seleccione un Modelo]");
+
+		if (modelos.size() != 0) {
+			modelos.forEach(modelo -> modelosValues.put(String.valueOf(modelo.getId()), modelo.getModelo()));
+		}
+
+		return modelosValues;
 	}
 }
--- src/main/java/com/udemy/controller/views/VehiculoViewController.java
+++ src/main/java/com/udemy/controller/views/VehiculoViewController.java
@@ -2,14 +2,22 @@ package com.udemy.controller.views;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.udemy.controller.common.view.ControllerView;
+import com.udemy.controller.common.view.LandingPages;
 import com.udemy.model.Vehiculo;
 import com.udemy.service.IMarcaService;
 import com.udemy.service.IVehiculoService;
@@ -34,7 +42,7 @@ public class VehiculoViewController extends ControllerView {
 	public String listarPorId(Model model, @PathVariable("id") Optional<Integer> id) {
 		model.addAttribute("marcas", marcaService.listar());
 
-		if (id.isPresent()) {			
+		if (id.isPresent()) {
 			model.addAttribute("titulo", "Modificar Vehículo");
 			model.addAttribute("vehiculoForm", service.listarPorId(id.get()));
 		} else {
@@ -42,5 +50,31 @@ public class VehiculoViewController extends ControllerView {
 			model.addAttribute("vehiculoForm", new Vehiculo());
 		}
 		return "vehiculos/form";
+	}
+
+	@PostMapping("/procesar")
+	public String salvar(@RequestParam("file") MultipartFile file, Model model,
+			@Valid @ModelAttribute("vehiculo") Vehiculo vehiculo, BindingResult bindingResult) {
+		try {
+			if (bindingResult.hasErrors()) {
+				return "conductor/form";
+			} else {
+				if (service.listarPorId(vehiculo.getId()) == null) {
+					/// service.crear(vehiculo);
+					return Landing(model, "Crear Conductor(a)",
+							"El Vehículo " + vehiculo.getModelo() + " Fue Creado creado con Exito", "form",
+							LandingPages.SUCCESS);
+				} else {
+					// service.modificar(vehiculo);
+					return Landing(model, "Modificar Conductor(a)",
+							"La Información del " + vehiculo.getModelo() + " Fue Modificada con Exito", "listar",
+							LandingPages.SUCCESS);
+				}
+			}
+		} catch (Exception e) {
+			return Landing(model, "Error Guardando Conductor(a)",
+					"Hubo un error al momento de guardar un Conductor(a) en: " + e.getMessage(), "listar",
+					LandingPages.ERROR);
+		}
 	}
 }
